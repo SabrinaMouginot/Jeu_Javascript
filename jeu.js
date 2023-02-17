f8f3fd5ea34dedb3477cae8dcffa2fd0362da40b
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-17 23:36:44 +0100

    Reconnaissance de l'obstacle

--- jeu.js
+++ jeu.js
@@ -1,4 +1,5 @@
 var perso = document.querySelector(".perso");
+var obstacles = document.querySelector(".obstacles");
 
 function sauter(){
     // function pour faire sauter le personnage
@@ -9,3 +10,11 @@ function sauter(){
         perso.classList.remove('animation'); 
     },500)
 }
+
+//verifier si  l'obstacle touche le personnage
+
+var verification = setInterval(function(){
+    var persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top")) ;
+    var obstaclesLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left")) ;
+
+},1)
